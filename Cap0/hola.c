3a29aa45caf4bad49fadafda25ede8ad22818769
IsaMathdez <isamatoshdez@gmail.com>
2024-09-06 20:16:41 -0400

    Version 4

--- Cap0/hola.c
+++ Cap0/hola.c
@@ -2,8 +2,12 @@
 
 int main(){
 
+    int i;
     printf("Hola Mundo Feo \n");
     printf("Aqui estamos modificando el archivo \n");
-    printf("Esta es la 3ra modificacion");
+    printf("Esta es la 3ra modificacion \n");
+    printf("Dame un numero \n");
+    scanf("%i", &i);
+    printf("El numero es %i.", i);
 
 }
